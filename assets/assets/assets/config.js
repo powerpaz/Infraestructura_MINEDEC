c2917536d31fa9cf8062ee3136fe2345297e5fe7
powerpaz <61800125+powerpaz@users.noreply.github.com>
2025-10-30 11:36:32 -0500

    Create config.js

---
+++ assets/assets/assets/config.js
@@ -0,0 +1,5 @@
+// assets/config.js (solo para pruebas locales; NO commitear con claves reales)
+window.__CONF__ = {
+  SUPABASE_URL: "https://TU_PROJECT.supabase.co",
+  SUPABASE_ANON_KEY: "TU_ANON_KEY_PUBLICA"
+};
