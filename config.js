931616d39faf03fa2639a7b3de46ca46a5e396ea
powerpaz <61800125+powerpaz@users.noreply.github.com>
2025-10-31 11:05:27 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+  CSV: {
+    instituciones: "tabla_1.csv",
+    tabla2: "Tabla2.csv",
+    tabla3: "Tabla3_clean.csv",
+    tabla4: "Tabla4_clean.csv"
+  },
+  GEOJSON: "provincias.geojson",
+  LAYER_STYLE: {
+    tabla1: { color: "#4cc9f0" },
+    tabla2: { color: "#b5179e" },
+    tabla3: { color: "#f77f00" },
+    tabla4: { color: "#43aa8b" }
+  }
+};
